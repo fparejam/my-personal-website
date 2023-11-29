ffc3f847f925b8884bc33e0f544f41a35d83ad60
fparejam <fparejamayo@gmail.com>
2023-11-29 23:12:43 +0100

    feat: implemented stackai chatbot with gpt4 llm with personal cv as knowledge base

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Profession from "../components/Profession";
 import ProfilePicture from "../components/ProfilePicture";
 import Footer from "../components/Footer";
 import ThemeToggle from "../components/ThemeToggle";
+import Stack from "react-stackai";
 
 const HomePage = () => {
   const [darkMode, setDarkMode] = useState(false);
@@ -15,6 +16,7 @@ const HomePage = () => {
       <ProfilePicture />
       <h1 className={styles.title}>Hi! I&apos;m Fernando Pareja. I&apos;m a </h1>
       <h2 className={styles.subtitle}><Profession/></h2>
+      <Stack project="https://www.stack-ai.com/embed/48f920da-30b7-4a57-a4f5-beda092b201b/34670df7-6c63-4ac0-aca9-512ff0f8e983/6567a58c67dda1d3a0fb6727" />
       <Footer darkMode={darkMode} />
     </div>
   );
